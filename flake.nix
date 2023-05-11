f7096d04cb223deb0b652f06abcbcccce7a7c426
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-05-11 18:46:03 -0400

    support more systems

--- flake.nix
+++ flake.nix
@@ -23,7 +23,11 @@
     cosmwasm-src,
   }:
     flake-utils.lib.eachSystem
-      [ "x86_64-linux" ]
+      [ "x86_64-linux"
+        "aarch64-linux"
+        "x86-64-darwin"
+        "aarch64-darwin"
+      ]
       (system:
         let
           pkgs = import nixpkgs {
